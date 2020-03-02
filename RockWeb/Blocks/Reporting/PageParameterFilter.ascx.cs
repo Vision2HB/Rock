dea9269830adf9e85149427bb834defb589f62db
Taylor Cavaletto <tjcavaletto@gmail.com>
2020-03-02 15:33:41 -0700

    - Reorganized methods

--- RockWeb/Blocks/Reporting/PageParameterFilter.ascx.cs
+++ RockWeb/Blocks/Reporting/PageParameterFilter.ascx.cs
@@ -16,6 +16,7 @@ using Rock.Web.UI;
 using System.Web;
 using Rock.Security;
 using Rock.Field.Types;
+using System.Collections.Specialized;
 
 namespace RockWeb.Blocks.Reporting
 {
@@ -69,12 +70,18 @@ namespace RockWeb.Blocks.Reporting
         #endregion
 
         #region Base Control Methods
+
+        /// <summary>
+        /// Restores the view-state information from a previous user control request that was saved by the <see cref="M:System.Web.UI.UserControl.SaveViewState" /> method.
+        /// </summary>
+        /// <param name="savedState">An <see cref="T:System.Object" /> that represents the user control state to be restored.</param>
         protected override void LoadViewState( object savedState )
         {
             base.LoadViewState( savedState );
 
             CurrentParameters = ViewState["CurrentParameters"] as Dictionary<string, object>;
         }
+
         /// <summary>
         /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
         /// </summary>
@@ -381,7 +388,125 @@ namespace RockWeb.Blocks.Reporting
             mdFilter.Show();
         }
 
+        /// <summary>
+        /// Handles the Save filters event.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void mdFilter_SaveClick( object sender, EventArgs e )
+        {
+            Rock.Model.Attribute attribute = null;
+
+            // sets the attribute to use the "CustomSetting" attribute
+            var entityTypeId = EntityTypeCache.Get( Rock.SystemGuid.EntityType.ATTRIBUTE ).Id;
+            var entityId = EntityTypeCache.Get( Rock.SystemGuid.EntityType.BLOCK ).Id;
+
+            edtFilter.CategoryIds = new CategoryService( new RockContext() ).Queryable().Where( c => c.Name == "CustomSetting" &&
+                                                                                                c.EntityTypeId == entityTypeId &&
+                                                                                                c.EntityTypeQualifierColumn == "EntityTypeId" &&
+                                                                                                c.EntityTypeQualifierValue == entityId.ToString() )
+                                                                                        .Select( c => c.Id );
+
+            attribute = Helper.SaveAttributeEdits( edtFilter, _blockTypeEntityId, "Id", _block.Id.ToString() );
+
+            // Attribute will be null if it was not valid
+            if ( attribute == null )
+            {
+                return;
+            }
+
+            mdFilter.Hide();
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Binds the filter grid.
+        /// </summary>
+        private void BindGrid()
+        {
+            IQueryable<Rock.Model.Attribute> query = null;
+            var attributeService = new AttributeService( new RockContext() );
+
+            query = attributeService.Get( _blockTypeEntityId, "Id", _block.Id.ToString() );
+            gFilters.DataSource = query.OrderBy( a => a.Order ).ToList();
+            gFilters.DataBind();
+        }
+
         #endregion Settings
+        
+        #region Events
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            LoadFilters();
+        }
+
+        /// <summary>
+        /// Handles the btnFilter event.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnFilter_Click( object sender, EventArgs e )
+        {
+            NameValueCollection queryString = GenerateQueryString();
+
+            string url = Request.Url.AbsolutePath;
+
+            Guid? pageGuid = GetAttributeValue( AttributeKey.RedirectPage ).AsGuidOrNull();
+            if ( pageGuid.HasValue )
+            {
+                var page = PageCache.Get( pageGuid.Value );
+
+                url = System.Web.VirtualPathUtility.ToAbsolute( string.Format( "~/page/{0}", page.Id ) );
+            }
+
+            if ( queryString.AllKeys.Any() )
+            {
+                Response.Redirect( string.Format( "{0}?{1}", url, queryString ), false );
+            }
+            else
+            {
+                Response.Redirect( url, false );
+            }
+        }
+
+        /// <summary>
+        /// Handles the btnResetFilters event.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnResetFilters_Click( object sender, EventArgs e )
+        {
+            ResetFilters();
+        }
+
+        /// <summary>
+        /// Handles the SelectItem event from an ItemPicker (fake event to register the postback)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ItemPicker_SelectItem( object sender, EventArgs e )
+        {
+            //hopefully an xhr happens here
+        }
+
+        /// <summary>
+        /// Handles the ListControl Selected Index Changed event.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void Control_PreRender( object sender, EventArgs e )
+        {
+            // Update the internal URL querystring via postback call
+            PostBackUpdateQueryString();
+        }
+
+        #endregion
 
         #region Methods
 
@@ -464,7 +589,7 @@ namespace RockWeb.Blocks.Reporting
         {
             BuildControls();
 
-            System.Collections.Specialized.NameValueCollection queryString = GenerateQueryString();
+            NameValueCollection queryString = GenerateQueryString();
 
             if ( queryString.AllKeys.Any() )
             {
@@ -477,97 +602,21 @@ namespace RockWeb.Blocks.Reporting
         }
 
         /// <summary>
-        /// Binds the filter grid.
-        /// </summary>
-        private void BindGrid()
-        {
-            IQueryable<Rock.Model.Attribute> query = null;
-            var attributeService = new AttributeService( new RockContext() );
-
-            query = attributeService.Get( _blockTypeEntityId, "Id", _block.Id.ToString() );
-            gFilters.DataSource = query.OrderBy( a => a.Order ).ToList();
-            gFilters.DataBind();
-        }
-
-        /// <summary>
         /// Updates the internal Query String with control's selections
         /// </summary>
         private void PostBackUpdateQueryString()
         {
-            Dictionary<string, string> queryStringList = new Dictionary<string, string>();
-            if ( _block.Attributes != null )
-            {
-                foreach ( var attribute in _block.Attributes )
-                {
-                    Control control = phAttributes.FindControl( string.Format( "attribute_field_{0}", attribute.Value.Id ) );
-                    if ( control != null )
-                    {
-
-                        string value = attribute.Value.FieldType.Field.GetEditValue( control, attribute.Value.QualifierValues );
-
-                        if ( value.IsNotNullOrWhiteSpace() )
-                        {
-                            queryStringList.Add( attribute.Key, value );
-                        }
-                    }
-                }
-            }
+            var queryString = GenerateQueryString();
 
             //Change query string without redirect ( a little bit of a hack )
-            System.Reflection.PropertyInfo isreadonly = typeof( System.Collections.Specialized.NameValueCollection ).GetProperty( "IsReadOnly", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic );
+            System.Reflection.PropertyInfo isreadonly = typeof( NameValueCollection ).GetProperty( "IsReadOnly", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic );
 
             if ( Request != null )
             {
                 isreadonly.SetValue( Request.QueryString, false, null );
                 Request.QueryString.Clear();
-                foreach ( var qry in queryStringList )
-                {
-                    Request.QueryString.Set( qry.Key, qry.Value );
-                }
+                Request.QueryString.Add( queryString );
                 isreadonly.SetValue( Request.QueryString, true, null );
-            }
-        }
-
-        #endregion
-
-        #region Events
-
-        /// <summary>
-        /// Handles the BlockUpdated event of the control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected void Block_BlockUpdated( object sender, EventArgs e )
-        {
-            LoadFilters();
-        }
-
-        /// <summary>
-        /// Handles the btnFilter event.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected void btnFilter_Click( object sender, EventArgs e )
-        {
-            System.Collections.Specialized.NameValueCollection queryString = GenerateQueryString();
-
-            string url = Request.Url.AbsolutePath;
-
-            Guid? pageGuid = GetAttributeValue( AttributeKey.RedirectPage ).AsGuidOrNull();
-            if ( pageGuid.HasValue )
-            {
-                var page = PageCache.Get( pageGuid.Value );
-
-                url = System.Web.VirtualPathUtility.ToAbsolute( string.Format( "~/page/{0}", page.Id ) );
-            }
-
-            if ( queryString.AllKeys.Any() )
-            {
-                Response.Redirect( string.Format( "{0}?{1}", url, queryString ), false );
-            }
-            else
-            {
-                Response.Redirect( url, false );
             }
         }
 
@@ -575,7 +624,7 @@ namespace RockWeb.Blocks.Reporting
         /// Generates the query string.
         /// </summary>
         /// <returns></returns>
-        private System.Collections.Specialized.NameValueCollection GenerateQueryString()
+        private NameValueCollection GenerateQueryString()
         {
             var queryString = HttpUtility.ParseQueryString( String.Empty );
             foreach ( var parameter in CurrentParameters )
@@ -612,68 +661,7 @@ namespace RockWeb.Blocks.Reporting
             return queryString;
         }
 
-        /// <summary>
-        /// Handles the btnResetFilters event.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected void btnResetFilters_Click( object sender, EventArgs e )
-        {
-            ResetFilters();
-        }
-
-        /// <summary>
-        /// Handles the Save filters event.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        protected void mdFilter_SaveClick( object sender, EventArgs e )
-        {
-            Rock.Model.Attribute attribute = null;
-
-            // sets the attribute to use the "CustomSetting" attribute
-            var entityTypeId = EntityTypeCache.Get( Rock.SystemGuid.EntityType.ATTRIBUTE ).Id;
-            var entityId = EntityTypeCache.Get( Rock.SystemGuid.EntityType.BLOCK ).Id;
-
-            edtFilter.CategoryIds = new CategoryService( new RockContext() ).Queryable().Where( c => c.Name == "CustomSetting" &&
-                                                                                                c.EntityTypeId == entityTypeId &&
-                                                                                                c.EntityTypeQualifierColumn == "EntityTypeId" &&
-                                                                                                c.EntityTypeQualifierValue == entityId.ToString() )
-                                                                                        .Select( c => c.Id );
-
-            attribute = Helper.SaveAttributeEdits( edtFilter, _blockTypeEntityId, "Id", _block.Id.ToString() );
-
-            // Attribute will be null if it was not valid
-            if ( attribute == null )
-            {
-                return;
-            }
-
-            mdFilter.Hide();
-            BindGrid();
-        }
-
-        /// <summary>
-        /// Handles the SelectItem event from an ItemPicker (fake event to register the postback)
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ItemPicker_SelectItem( object sender, EventArgs e )
-        {
-            //hopefully an xhr happens here
-        }
-
-        /// <summary>
-        /// Handles the ListControl Selected Index Changed event.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        private void Control_PreRender( object sender, EventArgs e )
-        {
-            // Update the internal URL querystring via postback call
-            PostBackUpdateQueryString();
-        }
-
         #endregion
+
     }
 }
