77098bca006dfb3cb9aaf3a4f5db819ad805f774
Sam Crisp <44408754+sam-crisp@users.noreply.github.com>
2020-08-11 14:44:17 -0500

    NPM GroupLaunch Stuff

---
+++ RockWeb/Plugins/com_northpoint/GroupLaunch/GroupLaunch.ascx
@@ -0,0 +1,98 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="GroupLaunch.ascx.cs" Inherits="RockWeb.Plugins.com_northpoint.GroupLaunch.GroupLaunch" %>
+
+<asp:UpdatePanel ID="upList" runat="server">
+    <ContentTemplate>
+
+        <asp:Panel ID="pnlContent" runat="server">
+            <asp:HiddenField ID="glGroupId" runat="server" />
+            <asp:HiddenField ID="glGroupCollectionId" runat="server" />
+            <asp:HiddenField ID="glGroupName" runat="server" />
+
+            <div id="pnlGroupLaunch" runat="server">
+                <div class="panel panel-block">
+
+                    <div class="panel-heading">
+                        <h1 class="panel-title pull-left">
+                            <i class="fa fa-clipboard-list"></i>
+                            <asp:Literal ID="lHeading" runat="server" Text="Group Launch" />
+                        </h1>
+
+                        <div class="panel-labels">
+                            <span runat="server" id="spanSyncLink" visible="false" data-toggle="popover" class="label label-info js-sync-popover" data-trigger="hover click focus" data-html="true" data-placement="left" data-delay="{&quot;hide&quot;: 1500}"><i class='fa fa-exchange'></i></span>&nbsp;           
+                        </div>
+                    </div>
+
+                    <div class="panel-body">
+                        <div id="pnlConnectReadOnlyDetails" runat="server">
+                            <style>
+                                .actions {
+                                    margin-top: 2rem;
+                                }
+                            </style>
+                            <% if (IsConnected() ) { %>
+                                <div>
+                                    <p>Status: <Rock:HighlightLabel ID="ConnectedHighlight" runat="server" LabelType="Success" Text="Connected" /></p>
+                                    <p>Name: <%= groupLaunchGroupName %></p>
+                                    <div class="actions">
+                                        <asp:HyperLink 
+                                            ID="ManageGroupLaunchLink"
+                                            runat="server"
+                                            Text="Manage in GroupLaunch"
+                                            Class="btn btn-primary"
+                                            Target="_blank"
+                                            >
+                                        </asp:HyperLink>
+                                        
+                                        <% if ( IsFinalized() )
+                                            { %>
+                                            <asp:LinkButton ID="ReConnectButton" runat="server" Text="Re-Publish" CssClass="btn btn-primary" OnClick="btnConnect_Click" CausesValidation="false" />
+                                        
+                                        <% }
+                                        else
+                                        { %>
+                                        </div>
+                                        <div style="margin-top: 1em;">
+                                            <Rock:NotificationBox ID="nbConnected" runat="server" CssClass="padding-top-md" NotificationBoxType="Info" Title="Please DO NOT Make Any Changes To This Group"
+                                                Text="It is currently open for registration in GroupLaunch and will interfere with the synchronization between systems." />
+                                        <% } %>
+                                    </div>
+                                </div>
+                            <% } else { %>
+                                <div>
+                                    Status: <Rock:HighlightLabel ID="UnconnectedHighlight" runat="server" LabelType="Default" Text="Unconnected" />   
+                                    <br /><br />
+                                    <div class="actions">
+                                        <asp:LinkButton ID="ConnectButton" runat="server" Text="Connect" CssClass="btn btn-primary" OnClick="btnConnect_Click" CausesValidation="false" />
+                                    </div>
+                                </div>
+                            <% } %>
+                        </div>
+
+                        <div id="pnlConnectDetails" runat="server">
+                            <Rock:NotificationBox ID="nbWarning" runat="server" Dismissable="true" Visible="false" />
+                            <Rock:RockDropDownList ID="ddlGroupCollections" runat="server" Label="Group Collections" Help="Pick the Group Collection" Required="true" />
+                            <Rock:RockDropDownList ID="ddlGroupLeaderRole" runat="server" Label="Rock Leader Role" Help="Select the Rock Group Role that Leaders will map to from GroupLaunch" Required="true" />
+                            <Rock:RockTextBox ID="groupNameText" runat="server" Label="Group Name" Help="Name of the group in GroupLaunch" />
+
+                            <Rock:ModalDialog ID="mdConfirmRePublish" runat="server" Title="Confirm GroupLaunch Remove and Create"
+                                OnSaveClick="mdConfirmRePublish_SaveClick" OnCancelScript="clearActiveDialog();" SaveButtonText="Re-Publish">
+                                <Content>
+                                    <p>This will delete the existing group in GroupLaunch before re-creating with current Rock Group.</p>
+                                    <p>Are you sure you want to re-publish to GroupLaunch?</p>
+                                </Content>
+                            </Rock:ModalDialog>
+
+                            <div class="actions">
+                                <asp:LinkButton ID="btnConnectPublish" runat="server" Text="Publish" CssClass="btn btn-primary" OnClick="btnPublish_Click" CausesValidation="false" />
+                                <asp:LinkButton ID="btnRePublish" runat="server" Text="Re-Publish" CssClass="btn btn-primary" OnClick="btnRePublish_Click" Visible="false" />
+                                <asp:LinkButton ID="btnCancelConnect" runat="server" Text="Cancel" CssClass="btn" OnClick="btnCancelConnect_Click" CausesValidation="false" />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ RockWeb/Plugins/com_northpoint/GroupLaunch/GroupLaunch.ascx.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using com_northpoint.GroupLaunch;
+using com_northpoint.GroupLaunch.Models;
+
+
+using Rock.Attribute;
+using System.ComponentModel;
+using Group = com_northpoint.GroupLaunch.Models.Group;
+using Rock.Security;
+
+namespace RockWeb.Plugins.com_northpoint.GroupLaunch
+{
+
+    [DisplayName( "Group Launch (Action Bar)" )]
+    [Category( "North Point Ministries > GroupLaunch" )]
+    [Description( "Lock and Publish A Group To GroupLaunch and check GroupLaunch Service for any updates on finalized status" )]
+
+    [GroupTypesField( "Group Type Visibity", "Show this block for these group types", false )]
+    [TextField("GroupLaunch Service URL","Full URL to the api service. ", true, "https://test.launch.grouplink.org")]
+
+    public partial class GroupLaunch : Rock.Web.UI.RockBlock
+    {
+        private GroupLaunchService groupLaunchService;
+        private Group group;
+        private int currentRockGroupId;
+        public string groupLaunchGroupName;
+        private bool isGroupLaunchGroup = false;
+
+        public bool IsConnected()
+        {
+            return group != null;
+        }
+
+        public bool IsFinalized()
+        {
+            return group.Finalized;
+        }
+
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+        }
+
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad(e);
+
+            currentRockGroupId = PageParameter( "GroupId" ).AsInteger();
+            groupLaunchService = new GroupLaunchService( GetAttributeValue("GroupLaunchServiceURL") );
+
+            
+
+            if ( !Page.IsPostBack )
+            {
+                pnlGroupLaunch.Visible = false;
+                pnlConnectDetails.Visible = false;
+                mdConfirmRePublish.Hide();
+                pnlConnectReadOnlyDetails.Visible = true;
+                CheckBlockAppliesToGroup();
+                SetBlockVisibility();
+                CheckForExistingConnection();
+            }
+            
+        }
+
+        private void CheckBlockAppliesToGroup()
+        {
+            var groupTypeGuids = this.GetAttributeValue( "GroupTypeVisibity" ).SplitDelimitedValues().AsGuidList();
+
+            Rock.Model.Group group = new GroupService( new RockContext() )
+                .Queryable()
+                .Include( g => g.GroupType )
+                .Where( g => g.Id == currentRockGroupId )
+                .FirstOrDefault();
+
+            isGroupLaunchGroup = group.IsNotNull<Rock.Model.Group>() && groupTypeGuids.Contains( group.GroupType.Guid );
+        }
+
+        private void SetBlockVisibility()
+        {
+            if ( isGroupLaunchGroup )
+            {
+                pnlGroupLaunch.Visible = true;
+
+                // Check for auth token global attributes (if not available, throw up message)
+                if (GlobalAttributesCache.Value("NPMAuthUrl").IsNullOrWhiteSpace())
+                {
+                    UnconnectedHighlight.Text = "Global Attribute: NPMAuthURL Not Available";
+                    UnconnectedHighlight.Visible = true;
+                }
+                else if (GlobalAttributesCache.Value("NPMAuthClientId").IsNullOrWhiteSpace())
+                {
+                    UnconnectedHighlight.Text = "Global Attribute: NPMAuthClientId Not Available";
+                    UnconnectedHighlight.Visible = true;
+                }
+                else if (GlobalAttributesCache.Value("NPMAuthClientSecret").IsNullOrWhiteSpace())
+                {
+                    UnconnectedHighlight.Text = "Global Attribute: NPMAuthClientSecret Not Available";
+                    UnconnectedHighlight.Visible = true;
+                }
+            }
+        }
+
+        private void CheckForExistingConnection()
+        {
+            if ( isGroupLaunchGroup )
+            {
+                var groupResponse = groupLaunchService.FindGroupByRockId( currentRockGroupId );
+
+                if ( groupResponse != null )
+                {
+                    group = groupResponse;
+
+                    ManageGroupLaunchLink.NavigateUrl = groupResponse.GroupLaunchAdminUrl;
+                    groupLaunchGroupName = group.Name;
+
+                    //Hidden Fields
+                    glGroupId.Value = group.Id.ToString();
+                    glGroupCollectionId.Value = group.CollectionId.ToString();
+                    glGroupName.Value = group.Name.ToString();
+
+                    // Check if group was finalized in GroupLaunch, allowing Rock to unfreeze group security
+                    if( group.Finalized )
+                    {
+                        UnFreezeRockGroup( currentRockGroupId );
+                    }
+                    else
+                    {
+                        // Else, lock down this Rock group
+                        FreezeRockGroup( currentRockGroupId );
+                    }
+                }
+            }
+            else
+            {
+                //Do Not Unfreeze a Rock Group just because you don't receive a group object back from GroupLaunch
+            }
+        }
+
+        protected void btnConnect_Click( object sender, EventArgs e )
+        {
+            PopulateGroupCollectionDropdown();
+            PopulateGroupLeaderRoleDropdown();
+            ShowConnectDetails();
+        }
+
+        protected void btnCancelConnect_Click( object sender, EventArgs e )
+        {
+            pnlConnectReadOnlyDetails.Visible = true;
+            pnlConnectDetails.Visible = false;
+        }
+
+        protected void btnPublish_Click( object sender, EventArgs e )
+        {
+            string selectedGroupCollection = ddlGroupCollections.SelectedValue;
+            string groupName = groupNameText.Text;
+            string leaderRoleId = ddlGroupLeaderRole.SelectedValue;
+            ConnectToGroupLaunch( selectedGroupCollection, groupName, leaderRoleId );
+        }
+
+        private void ShowConnectDetails()
+        {
+            pnlConnectReadOnlyDetails.Visible = false;
+            pnlConnectDetails.Visible = true;
+
+            nbWarning.Visible = false;
+
+            
+
+            //If Re-Populating, change options
+            if ( glGroupId.Value.IsNotNullOrWhiteSpace() && glGroupName.Value.IsNotNullOrWhiteSpace() )
+            {
+                btnConnectPublish.Visible = false;
+                btnRePublish.Visible = true;
+
+                ddlGroupCollections.Enabled = false;
+                groupNameText.Enabled = false;
+                groupNameText.Text = glGroupName.Value;
+            }
+        }
+
+        private void PopulateGroupCollectionDropdown()
+        {
+            var groupCollections = groupLaunchService.GroupCollections();
+
+            if ( groupCollections != null )
+            {
+                ddlGroupCollections.DataSource = groupCollections;
+                ddlGroupCollections.DataTextField = "Name";
+                ddlGroupCollections.DataValueField = "Id";
+
+                ddlGroupCollections.DataBind();
+            }
+        }
+
+        private void PopulateGroupLeaderRoleDropdown()
+        {
+            var rockGroup = new GroupService( new RockContext() ).Get( currentRockGroupId );
+            //Get current group's group type roles where IsLeader is True
+            var groupLeaderRoles = rockGroup.GroupType.Roles.Where( r => r.IsLeader ).ToDictionary( r => r.Id, r => r.Name );
+
+            ddlGroupLeaderRole.DataSource = groupLeaderRoles;
+            ddlGroupLeaderRole.DataTextField = "Value";
+            ddlGroupLeaderRole.DataValueField = "Key";
+            ddlGroupLeaderRole.DataBind();
+
+            //Set Group Name
+            groupNameText.Text = rockGroup.Name;
+        }
+
+        private void ConnectToGroupLaunch( string groupCollectionId, string groupName, string leaderRoleId )
+        {
+            string warnings = "";
+            Group creationResponse = groupLaunchService.CreateGroup( groupCollectionId, currentRockGroupId, groupName, leaderRoleId, out warnings );
+
+            // If we get back a warning, post here
+            if ( warnings.IsNotNullOrWhiteSpace() )
+            {
+                nbWarning.Visible = true;
+                nbWarning.Text = warnings;
+            }
+
+            if ( creationResponse != null )
+            {
+                group = creationResponse;
+            }
+        }
+
+        private void RePublishGroupLaunch( string leaderRoleId )
+        {
+            string warnings = "";
+            Group creationResponse = groupLaunchService.ReCreateGroup( glGroupId.ValueAsInt(), glGroupCollectionId.ValueAsInt(), currentRockGroupId, leaderRoleId, out warnings );
+
+            // If we get back a warning, post here
+            if ( warnings.IsNotNullOrWhiteSpace() )
+            {
+                nbWarning.Visible = true;
+                nbWarning.Text = warnings;
+            }
+
+            if ( creationResponse != null )
+            {
+                group = creationResponse;
+            }
+        }
+
+        private void FreezeRockGroup( int rockGroupId )
+        {
+            var rockGroup = new GroupService( new RockContext() ).Get( currentRockGroupId );
+
+            Authorization.RefreshAction( rockGroup.TypeId, rockGroupId, Authorization.MANAGE_MEMBERS );
+
+            Authorization.RefreshAction( rockGroup.TypeId, rockGroupId, Authorization.EDIT );
+
+            // Check if Current Person has EDIT or MANAGE MEMBERS authorization, lets take it away by adding All Users Deny to Edit/ManageMembers
+            if ( rockGroup.IsAuthorized( Authorization.MANAGE_MEMBERS, this.CurrentPerson ) || rockGroup.IsAuthorized( Authorization.EDIT, this.CurrentPerson ) )
+            {
+                // See if rules already has a deny all for manage members 
+                var rules = Authorization.AuthRules( rockGroup.TypeId, rockGroupId, Authorization.MANAGE_MEMBERS );
+                if ( rules.Any( r => r.AllowOrDeny == 'D' && r.SpecialRole == SpecialRole.AllUsers ) )
+                {
+                    // Already has a Deny
+                }
+                else
+                {
+                    // Add DENY Rule for ALL USERS (Special Role)
+                    this.DenyAll( rockGroup, Authorization.MANAGE_MEMBERS );
+                }
+
+                // See if rules already has a deny all
+                var rulesEDIT = Authorization.AuthRules( rockGroup.TypeId, rockGroupId, Authorization.EDIT );
+                if( rulesEDIT.Any( r => r.AllowOrDeny == 'D' && r.SpecialRole == SpecialRole.AllUsers ))
+                {
+                    // Already has a Deny
+                }
+                else
+                {
+                    // Add DENY Rule for ALL USERS (Special Role)
+                    this.DenyAll( rockGroup, Authorization.EDIT );
+                }
+            }
+
+            // Add UI Text of "No Edits Allowed"
+            ConnectedHighlight.Text = "Connected: Active In GroupLaunch";
+        }
+
+        private void UnFreezeRockGroup( int rockGroupId )
+        {
+            var rockGroup = new GroupService( new RockContext() ).Get( currentRockGroupId );
+
+            Authorization.RefreshAction( rockGroup.TypeId, rockGroupId, Authorization.MANAGE_MEMBERS );
+
+            Authorization.RefreshAction( rockGroup.TypeId, rockGroupId, Authorization.EDIT );
+
+            // See if rules already has a deny all for manage members 
+            var rules = Authorization.AuthRules( rockGroup.TypeId, rockGroupId, Authorization.MANAGE_MEMBERS );
+            if ( rules.Any( r => r.AllowOrDeny == 'D' && r.SpecialRole == SpecialRole.AllUsers ) )
+            {
+                // Already has a Deny
+                foreach( var rule in rules.Where( r => r.AllowOrDeny == 'D' && r.SpecialRole == SpecialRole.AllUsers ) )
+                {
+                    this.RemoveRule( rule );
+                }
+            }
+
+            // See if rules already has a deny all
+            var rulesEDIT = Authorization.AuthRules( rockGroup.TypeId, rockGroupId, Authorization.EDIT );
+            if ( rulesEDIT.Any( r => r.AllowOrDeny == 'D' && r.SpecialRole == SpecialRole.AllUsers ) )
+            {
+                // Already has a Deny
+                foreach ( var rule in rulesEDIT.Where( r => r.AllowOrDeny == 'D' && r.SpecialRole == SpecialRole.AllUsers ) )
+                {
+                    this.RemoveRule( rule );
+                }
+            }
+            
+            // Add UI Text of "No Edits Allowed"
+            ConnectedHighlight.Text = "Connected: GroupLaunch Finalized";
+        }
+
+        private void DenyAll( ISecured entity, string action, RockContext rockContext = null )
+        {
+            rockContext = rockContext ?? new RockContext();
+
+            var authService = new AuthService( rockContext );
+
+            // Update the order for any existing rules in database
+            var order = 1;
+            foreach ( var existingAuth in authService
+                .GetAuths( entity.TypeId, entity.Id, action ) )
+            {
+                existingAuth.Order = order++;
+            }
+
+            // Add the new auth (with order of zero)
+            var auth = new Auth
+            {
+                EntityTypeId = entity.TypeId,
+                EntityId = entity.Id,
+                Order = 0,
+                Action = action,
+                AllowOrDeny = "D",
+                SpecialRole = SpecialRole.AllUsers
+            };
+
+            authService.Add( auth );
+
+            rockContext.SaveChanges();
+
+        }
+
+        private void RemoveRule( AuthRule rule, RockContext rockContext = null )
+        {
+            rockContext = rockContext ?? new RockContext();
+
+            var authService = new AuthService( rockContext );
+
+
+            // Add the new auth (with order of zero)
+            var auth = authService.Get( rule.Id );
+
+            authService.Delete( auth );
+
+            rockContext.SaveChanges();
+        }
+
+        protected void mdConfirmRePublish_SaveClick( object sender, EventArgs e )
+        {
+            mdConfirmRePublish.Hide();
+
+            string leaderRoleId = ddlGroupLeaderRole.SelectedValue;
+
+            RePublishGroupLaunch( leaderRoleId );
+        }
+
+        protected void btnRePublish_Click( object sender, EventArgs e )
+        {
+            // Show Are you Sure?? Dialog
+            mdConfirmRePublish.Show();
+        }
+    }
+}
+
