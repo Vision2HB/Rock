8d0ef3d65a6149f4bcc08c4bc0440aa90c53f6d6
Kevin Rutledge <kevin.rutledge@bemaservices.com>
2020-11-07 11:21:57 -0500

    Add back ends to vuex
    add age ranges initial call and updated the add new tags to filter out tags already added.

--- RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/store/index.js
+++ RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/store/index.js
@@ -3,7 +3,8 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const taglistUrl = '/backend/tagsList.json';
+const tagListUrl = '/backend/tagsList.json';
+const ageRangesUrl = '/backend/ageRanges.json';
 
 export default new Vuex.Store({
   state: {
@@ -22,44 +23,52 @@ export default new Vuex.Store({
             id:2
           }],
     ageRangeOptions: [],
-    // From TagList.Vue
-    taglist: [],
+    // From tagList.Vue
+    tagList: [],
     start: 0,
     stepSize: 15,
     step:2,
 
   },
   getters:{
-    
+    currentTagIds(state) {
+      return state.tagList.map(tag => tag.id)
+    },
     
     
     filterTags(state){
-      let filteredList = state.taglist;
+      let filteredList = state.tagList;
       let pulledIds = state.pulledTags.map(tag => tag.id);  
 
-      if(pulledIds && pulledIds.length > 0 ) {
-        filteredList = filteredList.filter(tag => pulledIds.includes(tag.id) == false)
-      }
+      if(filteredList.length > 0) {
+        if(pulledIds && pulledIds.length > 0 ) {
+          filteredList = filteredList.filter(tag => pulledIds.includes(tag.id) == false)
+        }
 
-      if(state.selectedGenders && state.selectedGenders.length > 0){
-        filteredList = filteredList.filter(tag => state.selectedGenders.includes(tag.gender.id) == true)
-      }
+        if(state.selectedGenders && state.selectedGenders.length > 0){
+          filteredList = filteredList.filter(tag => state.selectedGenders.includes(tag.gender.id) == true)
+        }
 
-      if(state.selectedAgeRanges && state.selectedAgeRanges.length > 0){
-        filteredList = filteredList.filter(tag => state.selectedAgeRanges.includes(tag.ageRange.id) == true)
-      }
-      if(state.selectedCampus && state.selectedCampus> 0){
-        filteredList = filteredList.filter(tag => state.selectedCampus == tag.campusId)
-      }
-      return filteredList.slice(0,state.step * state.stepSize);
-      
+        if(state.selectedAgeRanges && state.selectedAgeRanges.length > 0){
+          filteredList = filteredList.filter(tag => state.selectedAgeRanges.includes(tag.ageRange.id) == true)
+        }
+        if(state.selectedCampus && state.selectedCampus> 0){
+          filteredList = filteredList.filter(tag => state.selectedCampus == tag.campusId)
+        }
+        return filteredList.slice(0,state.step * state.stepSize);
+    }
     },
   },
   mutations: {
+    // Initiated by the get tags action to concat the results into the tagsList store item.
     updateTags(state, tags) {
-      state.taglist = tags;
+        state.tagList = state.tagList.concat(tags);
     },
-
+    //Called by the initializestore action to update the store's age range options.
+    addAgeRanges(state, ageRanges){
+      console.log(ageRanges)
+      state.ageRangeOptions = ageRanges
+    },
     //Are called by the changes in the app.vue selects and update the store values.
     updateSelectedGenders(state,selectedGenders){
       state.selectedGenders = selectedGenders;
@@ -71,19 +80,47 @@ export default new Vuex.Store({
       state.selectedCampus = selectedCampus;
     },
     addPulledtag(state, tag){
-      state.pulledTags.push(tag);
-    }
+      state.pulledTags.push(tag.id);
+    },
+
+
   },
   actions: {
-    // action get tags from the back end  the taglistURl is set above so it can be changed from development to production.
-    async getTags({commit}){
-      console.log('action started')
-      let response = await fetch(taglistUrl);
-      console.log(response)
-      let tags  = await response.json(); 
-      console.log(tags)
-      commit('updateTags',tags)
+    // action get tags from the back end  the tagListURl is set above so it can be changed from development to production. 
+    // the new tags are filtered for any tags that have already been downloaded and only new tags are added.
+    async getTags({commit, getters}){
+        let response = await fetch(tagListUrl);
+        let tags  = await response.json(); 
+        const currentTags = getters.currentTagIds;
+        
+        console.log(tags.length)
+        if(currentTags.length > 0 ){
+          tags = tags.filter( tag => currentTags.includes(tag.id) === false );
+        } 
+        console.log(tags)
+        commit('updateTags',tags)
 
+      },
+
+    // Get Age ranges
+    async getAgeRanges({commit}){
+      let response = await fetch(ageRangesUrl);
+      let ageRanges = await response.json()
+      commit('addAgeRanges',ageRanges)
+
+    },
+
+    //Called when the app is Created and gets the first set of tags and pulled tags from local storage.
+    initializeStore({commit, dispatch,}){
+        // get Age Range Options
+        dispatch('getAgeRanges')
+
+        // Gets new tags from endpont using the getTags store action
+        dispatch('getTags')
+        
+        // Get Tags From Local StorageId and add each to the vuex store pulled Tags
+        const tagList = JSON.parse(localStorage.getItem('pulledTags'))
+        tagList.forEach(tag => commit('addPulledtag',{id: tag}))
     }
   },
   modules: {
