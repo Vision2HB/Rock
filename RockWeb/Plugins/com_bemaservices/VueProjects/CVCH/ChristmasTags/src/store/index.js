99c204449b58ad8cea1e63bcba45dc749e9d4604
Kevin Rutledge <kevin.rutledge@bemaservices.com>
2020-11-07 07:57:31 -0500

    Adding the Tag List filters to a store getter.

--- RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/store/index.js
+++ RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/store/index.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // From App.Vue
     pulledTags:[],
     selectedGenders:[],
     selectedAgeRanges:[],
@@ -19,6 +20,38 @@ export default new Vuex.Store({
             id:2
           }],
     ageRangeOptions: [],
+    // From TagList.Vue
+    taglist: [],
+    start: 0,
+    stepSize: 15,
+    step:2,
+
+  },
+  getters:{
+    
+    
+    
+    filterTags(){
+      let filteredList = this.$state.taglist;
+      let pulledIds = this.$state.pulledTags.map(tag => tag.id);  
+
+      if(pulledIds && pulledIds.length > 0 ) {
+        filteredList = filteredList.filter(tag => pulledIds.includes(tag.id) == false)
+      }
+
+      if(this.$state.selectedGenders && this.$state.selectedGenders.length > 0){
+        filteredList = filteredList.filter(tag => this.$state.selectedGenders.includes(tag.gender.id) == true)
+      }
+
+      if(this.$state.selectedAgeRanges && this.$state.selectedAgeRanges.length > 0){
+        filteredList = filteredList.filter(tag => this.$state.selectedAgeRanges.includes(tag.ageRange.id) == true)
+      }
+      if(this.$state.selectedCampus && this.$state.selectedCampus> 0){
+        filteredList = filteredList.filter(tag => this.$state.selectedCampus == tag.campusId)
+      }
+      return filteredList.slice(0,this.$state.step * this.$state.stepSize);
+      
+    },
   },
   mutations: {
   },
