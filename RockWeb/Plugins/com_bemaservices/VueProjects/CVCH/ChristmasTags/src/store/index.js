a26dfcc5438190ea47af18345ddc629dbfa961d5
Kevin Rutledge <kevin.rutledge@bemaservices.com>
2020-11-07 07:48:21 -0500

    Move data from the app.vue compoenent to the store

--- RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/store/index.js
+++ RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/store/index.js
@@ -5,6 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    pulledTags:[],
+    selectedGenders:[],
+    selectedAgeRanges:[],
+    selectedCampus: personCampus,
+    pulledTags:[],
+    campusOptions:campuses,
+    genderOptions: [{
+            description:'Boy',
+            id:1
+          },{
+            description:'Girl',
+            id:2
+          }],
+    ageRangeOptions: [],
   },
   mutations: {
   },
