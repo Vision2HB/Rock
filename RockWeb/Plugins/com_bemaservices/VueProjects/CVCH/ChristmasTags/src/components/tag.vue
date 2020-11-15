7cf8c95515549cdfc3d95f95f7a2984bba11d42e
Kevin Rutledge <kevin.rutledge@bemaservices.com>
2020-11-15 11:18:15 -0500

    Add Notes
    Add notees to tag component and when financial donation is required to show the suggested donation amount.

--- RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/components/tag.vue
+++ RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/components/tag.vue
@@ -25,7 +25,17 @@
                         <v-btn 
                          color="accent" 
                          elevation="2"
-                         @click="addCard(tag, 'tag' + tag.id)">
+                         @click="addCard(tag, 'tag' + tag.id)"
+                         v-if="tag.requireFinancialDonation == true"
+                         >
+                         ${{tag.suggestedDonation}} Donation
+                         </v-btn>
+                         <v-btn 
+                         color="accent" 
+                         elevation="2"
+                         @click="addCard(tag, 'tag' + tag.id)"
+                         v-else
+                         >
                          Pull Tag
                          </v-btn>
                         <div v-if="selectedColor.shape != ''" class="rule-shape" v-html="selectedColor.shape"></div>
@@ -40,7 +50,6 @@
 
 <script>
 // Import the EventBus we just created.
-import { EventBus } from '../modules/event-bus.js';
 import { gsap } from "gsap";
 
 export default {
@@ -52,12 +61,6 @@ export default {
             default:"Make a difference in a family''s life."
         }
     },
-      created () {
-    window.addEventListener('scroll', this.handleScroll);
-    },
-    destroyed () {
-        window.removeEventListener('scroll', this.handleScroll);
-    },
     mounted(){
         this.tagColor(Math.floor(Math.random()*(4-0+1)+0) + this.tag.gender.id + this.tag.ageRange.id)
     },
@@ -68,10 +71,10 @@ export default {
         }
     },
     computed: {
-    colorOptions(){
-        return this.$store.state.colorOptions;
-      }
-  },
+        colorOptions(){
+            return this.$store.state.colorOptions;
+        }
+    },
     methods: {
         addCard(tag,ref) {
             //Change the tag to pulled and emit event
@@ -86,38 +89,37 @@ export default {
             
             //Get the location fo the tag
             let el = this.$refs[ref];
-            
             let elviewportOffset = el.getBoundingClientRect();
             let eltop = elviewportOffset.top;
             let elleft = elviewportOffset.left;
 
-
-            // let elviewportOffset = el.getBoundingClientRect();
-            // let eltop = el.getBoundingClientRect().top + 
-            //     el.ownerDocument.defaultView.pageYOffset;
-            // let elleft = elviewportOffset.left;
+            //Set the element to the current position so when you switch to fixed, it remains in place.
             el.style.top = eltop +'px';
             el.style.left = elleft + 'px';
             el.style.position = 'fixed';
             
-            
             //Calculate Movement value and add transform
             let moveLeft = (elleft + btnleft + 60) * -1;
             let moveDown = ((eltop - btntop ) * -1 -150);
-           
-            var tl = gsap.timeline({onComplete:() =>{  this.$store.commit('addPulledtag',tag)}})
-            // moveItem(el,elleft,eltop)
             
+            //Build the GSAP timeline and when complete call the commit to add the tag to the pulled tag list.
+            var tl = gsap.timeline({onComplete:() =>{  this.$store.commit('addPulledtag',tag)}})
+            
+            // Build the GSAP Animations that are addeed to the timeline. Move the item to the calcualted values and scale to 50% at the same time.
             tl.to(el, {x:moveLeft, y:moveDown, scale:.5})
+            
+            // Scale the item to 0 after move.
             tl.to(el, {scale:0},">")
             
+            //Activate the Animation
             tl.duration(1).play();
-            
         },
+        
         tagColor(i) {
-                
+                 //Uses the random number generated in the method above and pics a color option out of the three available using Modulo, and appends the color rules.
                  this.selectedColor = this.colorOptions[i % 3];
                  this.selectedColor.rulesClass = this.selectedColor.rules.join(' ');
+                 console.log(this.selectedColor)
         },
 
     }
--- RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/main.js
+++ RockWeb/Plugins/com_bemaservices/VueProjects/CVCH/ChristmasTags/src/main.js
@@ -1,19 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import { EventBus } from './modules/event-bus.js';
 import store from './store'
 
-// // Event listener for warranty transaction success
-// window.addEventListener('message', function (e) {
-//   if (e.data.event === 'transactioncomplete') {
-//       EventBus.$emit('transactionComplete', e.data.data);
-//   }
-// });
 
-let firstName = '{{CurrentPerson.NickName}}';
-let lastName = 'CurrentPerson.LastName';
-let email = '{{CurrentPerson.Email}}';
+// You can put any js variables you want to set from lava here. The current format has an api call to get the current person and fill the tags. however, I could have used the variables here an preloaded everything.  Be sure to update the vuex store or the component code to use these variables instead of the api call.
+
+  // let firstName = '{{CurrentPerson.NickName}}';
+  // let lastName = 'CurrentPerson.LastName';
+  // let email = '{{CurrentPerson.Email}}';
 
 
 Vue.config.productionTip = true
